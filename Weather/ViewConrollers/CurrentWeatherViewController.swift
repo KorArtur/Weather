1b5b5bf815a286e53d3eb61079061143aa3401ef
popov <popich2005@mail.ru>
2024-03-09 16:04:18 +0300

    v.6

--- Weather/ViewConrollers/CurrentWeatherViewController.swift
+++ Weather/ViewConrollers/CurrentWeatherViewController.swift
@@ -27,96 +27,64 @@ final class CurrentWeatherViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
         currentRevengeLabel.text = "Текущее место"
-        cityLabel.text = cities[0].title
-        temperatureLabel.text = """
-        \(cities[0].weathers[0].weatherByHours[0].weather.temperature)  
-        \(cities[0].weathers[0].weatherByHours[0].weather.type)
-        """
+        cityLabel.text = city.title
+        temperatureLabel.text = "\(city.weathers[0].weatherByHours[0].weather.temperature)  \(city.weathers[0].weatherByHours[0].weather.type.rawValue)"
         
-        weekLabel.text = "Погода на семь дней"
+        color()
         
+        weekLabel.text = "Погода на семь дней:"
         
-        let labels = [mondayLabel, tuesdayLabel, wednesdayLabel, thursdayLabel,  fridaуLabel, saturdayLabel, sundayLabel]
+//        
+//        let labels = [mondayLabel, tuesdayLabel, wednesdayLabel, thursdayLabel,  fridaуLabel, saturdayLabel, sundayLabel]
+//
+//        for cityIndex in 0..<labels.count {
+//            let weatherData = city.weathers
+//            
+//            var cityWeatherTexts = ""
+//            for weather in weatherData {
+//                let day = weather.day
+//                for hourWeather in weather.weatherByHours {
+//                    let type = hourWeather.weather.type
+//                    let temperature = hourWeather.weather.temperature
+//                    let highLowTemperature = hourWeather.weather.highLowTemperature
+//                    
+//                    cityWeatherTexts += "\(day): \(type) \(temperature).... \(highLowTemperature)"
+//                }
+//            }
+//            
+//            labels[cityIndex]?.text = cityWeatherTexts
+//        }
+        
+        mondayLabel.text = "\(city.weathers[1].day.rawValue):      \(city.weathers[1].weatherByHours[1].weather.type.rawValue) Ночью: \(city.weathers[1].weatherByHours[1].weather.highLowTemperature.low)....Днем: \(city.weathers[1].weatherByHours[1].weather.highLowTemperature.high)"
+        
+        tuesdayLabel.text = "\(city.weathers[2].day.rawValue):      \(city.weathers[2].weatherByHours[2].weather.type.rawValue) Ночью: \(city.weathers[2].weatherByHours[2].weather.highLowTemperature.low)....Днем: \(city.weathers[2].weatherByHours[2].weather.highLowTemperature.high)"
 
-        for cityIndex in 0..<cities.count {
-            let weatherData = cities[cityIndex].weathers
-            
-            var cityWeatherTexts = ""
-            for weather in weatherData {
-                let day = weather.day
-                for hourWeather in weather.weatherByHours {
-                    let type = hourWeather.weather.type
-                    let temperature = hourWeather.weather.temperature
-                    let highLowTemperature = hourWeather.weather.highLowTemperature
-                    
-                    cityWeatherTexts += "\(day): \(type) \(temperature).... \(highLowTemperature)"
-                }
-            }
-            
-            labels[cityIndex]?.text = cityWeatherTexts
-        }
+        wednesdayLabel.text = "\(city.weathers[3].day.rawValue):      \(city.weathers[3].weatherByHours[3].weather.type.rawValue) Ночью: \(city.weathers[3].weatherByHours[3].weather.highLowTemperature.low)....Днем: \(city.weathers[3].weatherByHours[3].weather.highLowTemperature.high)"
+
+        thursdayLabel.text = "\(city.weathers[4].day.rawValue):      \(city.weathers[4].weatherByHours[4].weather.type.rawValue) Ночью: \(city.weathers[4].weatherByHours[4].weather.highLowTemperature.low)....Днем: \(city.weathers[4].weatherByHours[4].weather.highLowTemperature.high)"
         
-//        mondayLabel.text = """
-//        \(cities[1].weathers[1].day)
-//        \(cities[1].weathers[1].weatherByHours[1].weather.type)
-//        \(cities[1].weathers[1].weatherByHours[1].weather.temperature)....
-//        \(cities[1].weathers[1].weatherByHours[1].weather.highLowTemperature)
-//        """
-//        
-//        tuesdayLabel.text = """
-//        \(cities[2].weathers[2].day)
-//        \(cities[2].weathers[2].weatherByHours[2].weather.type)
-//        \(cities[2].weathers[2].weatherByHours[2].weather.temperature)....
-//        \(cities[2].weathers[2].weatherByHours[2].weather.highLowTemperature)
-//        """
-//        
-//        wednesdayLabel.text = """
-//        \(cities[3].weathers[3].day)
-//        \(cities[3].weathers[3].weatherByHours[3].weather.type)
-//        \(cities[3].weathers[3].weatherByHours[3].weather.temperature)....
-//        \(cities[3].weathers[3].weatherByHours[3].weather.highLowTemperature)
-//        """
-//        
-//        thursdayLabel.text = """
-//        \(cities[4].weathers[4].day)
-//        \(cities[4].weathers[4].weatherByHours[4].weather.type)
-//        \(cities[4].weathers[4].weatherByHours[4].weather.temperature)....
-//        \(cities[4].weathers[4].weatherByHours[4].weather.highLowTemperature)
-//        """
-//        
-//        fridaуLabel.text = """
-//        \(cities[5].weathers[5].day)
-//        \(cities[5].weathers[5].weatherByHours[5].weather.type)
-//        \(cities[5].weathers[5].weatherByHours[5].weather.temperature)....
-//        \(cities[5].weathers[5].weatherByHours[5].weather.highLowTemperature)
-//        """
-//        
-//        saturdayLabel.text = """
-//        \(cities[6].weathers[6].day)
-//        \(cities[6].weathers[6].weatherByHours[6].weather.type)
-//        \(cities[6].weathers[6].weatherByHours[6].weather.temperature)....
-//        \(cities[6].weathers[6].weatherByHours[6].weather.highLowTemperature)
-//        """
-//        
-//        sundayLabel.text = """
-//        \(cities[7].weathers[7].day)
-//        \(cities[7].weathers[7].weatherByHours[7].weather.type)
-//        \(cities[7].weathers[7].weatherByHours[7].weather.temperature)....
-//        \(cities[7].weathers[7].weatherByHours[7].weather.highLowTemperature)
-//        """
+        fridaуLabel.text = "\(city.weathers[5].day.rawValue):      \(city.weathers[5].weatherByHours[5].weather.type.rawValue) Ночью: \(city.weathers[5].weatherByHours[5].weather.highLowTemperature.low)....Днем: \(city.weathers[5].weatherByHours[5].weather.highLowTemperature.high)"
+        
+        saturdayLabel.text = "\(city.weathers[6].day.rawValue):      \(city.weathers[6].weatherByHours[6].weather.type.rawValue) Ночью: \(city.weathers[6].weatherByHours[6].weather.highLowTemperature.low)....Днем: \(city.weathers[6].weatherByHours[6].weather.highLowTemperature.high)"
+        
+        sundayLabel.text = "\(city.weathers[0].day.rawValue):      \(city.weathers[0].weatherByHours[0].weather.type.rawValue) Ночью: \(city.weathers[0].weatherByHours[0].weather.highLowTemperature.low)....Днем: \(city.weathers[0].weatherByHours[0].weather.highLowTemperature.high)"
         
     }
+    func color(){
     
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
+        switch city.weathers[0].weatherByHours[0].weather.type {
+        case .sunny:
+            view.backgroundColor = .systemYellow
+        case .cloudy:
+            view.backgroundColor = .systemGray2
+        case .rainy:
+            view.backgroundColor = .systemCyan
+        case .windy:
+            view.backgroundColor = .systemGray4
+       
+        }
+    }
     
 }
